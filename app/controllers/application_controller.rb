a1311b5870f7ff2acff23eb229759794e54728bf
mihiro <f.mihiroxx@gmail.com>
2020-04-21 12:15:41 +0900

    Fux bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
+  end
 end
