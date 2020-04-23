9976e1268cc55ebfdfd7f757bb4d3b27a1d9f412
mihiro <f.mihiroxx@gmail.com>
2020-04-23 19:04:30 +0900

    Create ReviewController

---
+++ app/controllers/guest_reviews_controller.rb
@@ -0,0 +1,45 @@
+class GuestReviewsController < ApplicationController
+
+    def create
+        # Step 1:Check if the reservation exist (room_id, host_id, guest_id)
+
+        # Step 2:Check if the current host already reviewd the guest in this reservation.
+
+        @reservation = Reservation.where(
+                            id: guest_review_params[:reservation_id],
+                            room_id: guest_review_params[:room_id]
+                            ).first
+
+        if !@reservation.nil? && @reservation.room.user.id == guest_review_params[:host_id].to_i
+
+            @has_reviewd = GuestReview.where(
+                            reservation_id: @reservation.id,
+                            host_id: guest_review_params[:host_id]
+                            ).first
+
+            if @has_reviewd.nil?
+                # Allow to review
+                @guest_reviewd = current_user.guest_reviews.create(guest_review_params)
+                flash[:success] = "Review created..."
+            else
+                # Already reviewed
+                flash[:success] = "You already reviewed this Reservation"
+            end
+        else
+            flash[:alert] = "Not found this reservation"
+        end
+        redirect_back(fallback_location: request.referer)
+    end
+
+    def destroy
+        @guest_review = Review.find(params[:id])
+        @guest_review.destroy
+
+        redirect_back(fallback_location: request.referer, notice: "Remove...!")
+    end
+
+    private
+        def guest_review_params
+            params.require(:guest_review).permit(:comment, :star, :room_id, :reservation_id, :host_id)
+        end
+end
