65665453918b48213e51a3b57aa1f966a51c31cf
mihiro <f.mihiroxx@gmail.com>
2020-04-24 11:47:03 +0900

    Create show reviews page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,11 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @rooms = @user.rooms
+
+        # display all the guest reviews to host (if this user is a host)
+        @guest_reviews = Review.where( type: "GuestReview", host_id: @user.id )
+
+        # display all the guest reviews to host (if this user is a guest)
+        @host_reviews = Review.where( type: "HostReview", guest_id: @user.id )
     end
 end
