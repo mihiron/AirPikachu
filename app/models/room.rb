ce238415062a21c25626120e3ee2c6fdb46bf2cd
mihiro <f.mihiroxx@gmail.com>
2020-04-24 13:16:04 +0900

    Fix model bug

--- app/models/room.rb
+++ app/models/room.rb
@@ -22,7 +22,7 @@ class Room < ApplicationRecord
     end
   end
 
-  def avarage_rating
-    guest_reviews.count == 0 ? 0 : guest_reviews.avarate(:star).round(2).to_i
+  def average_rating
+    guest_reviews.count == 0 ? 0 : guest_reviews.average(:star).round(2).to_i
   end
 end
