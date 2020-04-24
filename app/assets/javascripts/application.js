6c9281bee0a5f5794051f374c383aeb2658f03bd
mihiro <f.mihiroxx@gmail.com>
2020-04-24 12:56:14 +0900

    Fix JQuery bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require_tree .
 //= require jquery
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
 //= require bootstrap-sprockets
 //= require jquery-ui/datepicker
 //= require toastr
